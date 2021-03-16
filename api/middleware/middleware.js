e004c854e1b36abda39aab13ecb149240b53579a
ngwatso <ngwatson79@yahoo.com>
2021-03-16 12:17:21 -0400

    initial commit

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,19 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+	// ** DO YOUR MAGIC
+	console.log(req.method, req.URL, req.timeStamp);
+	next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+	// ** DO YOUR MAGIC
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+	// ** DO YOUR MAGIC
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+	// ** DO YOUR MAGIC
 }
 
-// do not forget to expose these functions to other modules
+// !! do not forget to expose these functions to other modules
--- api/server.js
+++ api/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
+// !! remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+// !! global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 module.exports = server;
