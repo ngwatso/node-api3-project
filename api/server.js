9ee80d25294dbb845cd36fc0c930656fb4854102
ngwatso <ngwatson79@yahoo.com>
2021-03-18 10:34:27 -0400

    moved .env file to root directory

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const Middleware = require('./middleware/middleware');
 
 // !! remember express by default cannot parse JSON in request bodies
 server.use(express.json());
-// !! global middlewares and the user's router need to be connected here
+// !! global middlewares and the user's router need to be connected here.
 server.use(helmet());
 server.use(Middleware.logger);
 
