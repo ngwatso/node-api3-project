834edb1422952995ce0637c51894e0fbec4ff6e5
ngwatso <ngwatson79@yahoo.com>
2021-03-18 20:20:18 -0400

    fixing server error

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 const router = require('./users/users-router');
 const Middleware = require('./middleware/middleware');
 
-// !! remember express by default cannot parse JSON in request bodies
+// !! remember express by default cannot parse JSON in request bodies.
 server.use(express.json());
 // !! global middlewares and the user's router need to be connected here.
 server.use(helmet());
